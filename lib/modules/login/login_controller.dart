6fa4ac4047311129f1f401bcfc0400fd15ccccd7
unknown <renato@volus.com>
2021-09-20 18:25:58 -0300

    inicial da autenticacao

--- lib/modules/login/login_controller.dart
+++ lib/modules/login/login_controller.dart
@@ -1,17 +1,23 @@
 import 'package:app_filmes/ui/loader/loader_mixin.dart';
+import 'package:app_filmes/ui/messages/messages_mixin.dart';
 import 'package:get/get.dart';
 
-class LoginController extends GetxController with LoaderMixin {
+class LoginController extends GetxController with LoaderMixin, MessagesMixin {
   final loading = false.obs;
+  final message = Rxn<MessageModel>();
   @override
   void onInit() {
     super.onInit();
     loadListener(loading);
+    messageListener(message);
   }
 
   Future<void> login() async {
     loading(true);
     await 2.seconds.delay();
     loading(false);
+    message(
+      MessageModel.error(title: 'Titulo Erro', message: 'Message Erro'),
+    );
   }
 }
